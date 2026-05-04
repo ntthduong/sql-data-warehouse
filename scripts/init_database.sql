a7f428eca2fbc543c076ece7e09123110ca7c882
Thuỳ Dương <ntthduongvn09@gmail.com>
2026-05-05 00:54:26 +0700

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -19,7 +19,7 @@ USE master;
 GO
 
 -- Drop and recreate 'DataWarehouse' database
-IF EXITS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
 BEGIN
 	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
 	DROP DATABASE DataWarehouse;
